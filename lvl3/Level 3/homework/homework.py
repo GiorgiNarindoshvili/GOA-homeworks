f0b080d8fbda815a727c6fd89fb3021fdaddf2fe
GiorgiNarindoshvili <giorgig20099@gmail.com>
2024-05-20 19:30:22 -0400

    Third commit

---
+++ lvl3/Level 3/homework/homework.py
@@ -0,0 +1,21 @@
+number1=10
+float1=10.5
+
+print(number1+float1)
+#გამოიტანა float რადგან float ის integer ად გადაკეთებისას 
+
+
+#task 2
+
+is_right= True
+is_wrong= False
+
+print(int(is_right))
+
+
+
+# task 3
+
+num1=20
+num2=24
+print(str(num1)) + print(str(num2))
