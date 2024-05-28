6f3fb20cfdfb0c2ef12cd723ab5b3d917f9664da
GiorgiNarindoshvili <giorgig20099@gmail.com>
2024-05-27 20:56:33 -0400

    fourth commit

---
+++ Level 4/Level 4/Classwork/classwork.py
@@ -0,0 +1,20 @@
+num1=5
+
+print(num1+1)
+
+#task 2
+
+#მნიშვნელობის გადაწერა ხდება მაშინ როდესაც ერთი და იგივე ცვლადის სახელს ორჯერ ან მეტჯერ ვანიჭებთ მნიშვნელობას
+#task 3:
+
+
+Balance=240
+
+Discount=[60]
+total=Balance-Discount
+
+print(total)
+
+
+name="Giorgi"
+name="Data"
