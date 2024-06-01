a137ab6336b987cd6afbb6694ba04ed28a3cddb5
GiorgiNarindoshvili <giorgig20099@gmail.com>
2024-06-01 12:56:48 -0400

    sixth commit

--- Level 4/Level 4/Classwork/classwork.py
+++ Level 4/Level 4/Classwork/classwork.py
@@ -10,11 +10,23 @@ print(num1+1)
 
 Balance=240
 
-Discount=[60]
-total=Balance-Discount
+Discount= 60
+total=(Balance-Discount)
 
 print(total)
 
 
 name="Giorgi"
-name="Data"
+name="Data" 
+
+payment_status = "returned"
+payment_status = "paid"
+print(payment_status)
+
+a = 3
+a = 5
+a = 7
+print(a)
+
+
+
